210f9e88eb53cf3f03edf435a95d605754fc586b
leo.matt.547 <leo.matt.547@gmail.com>
2022-02-06 11:40:47 +0700

    adding backend registration

---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/rs/cors"
+)
+
+type Parent struct {
+	gorm.Model
+	Nama    	string
+	Email		string
+	Password	string
+	Childs  	[]Child
+}
+  
+type Child struct {
+	gorm.Model
+	Nama    	string
+	Email		string
+	Password	string
+	ParentID	int
+}
+
+var db *gorm.DB
+
+var err error
+
+
+var (
+    parents = []Parent{
+        {Nama: "Jimmy Johnson", Email: "Jimmy@gmail.com", Password:"pplkel37"},
+        {Nama: "Howard Hills",  Email: "Howard@gmail.com", Password:"pplkel37"},
+        {Nama: "Craig Colbin",  Email: "Craig@gmail.com", Password:"pplkel37"},
+    }
+
+    childs = []Child{
+        {Nama: "Jimmy Junior", Email: "Jimmy_jr@gmail.com", Password:"pplkel37", ParentID:1},
+        {Nama: "Howard Junior",  Email: "Howard_jr@gmail.com", Password:"pplkel37", ParentID:2},
+        {Nama: "Craig Junior",  Email: "Craig_jr@gmail.com", Password:"pplkel37", ParentID:3},
+
+    }
+)
+
+func main() {	
+	router := mux.NewRouter()
+	db, err = gorm.Open("postgres", "host=ec2-52-213-119-221.eu-west-1.compute.amazonaws.com port=5432 user=rdulpadeipmwvr dbname=d93bbe48ni70dp sslmode=disable password=2cf4c3b493be5216e5309be9837d987e6be5294c696314809eed1702a230d15b")
+/*	dsn := "host=ec2-52-213-119-221.eu-west-1.compute.amazonaws.com user=rdulpadeipmwvr password=2cf4c3b493be5216e5309be9837d987e6be5294c696314809eed1702a230d15b dbname=d93bbe48ni70dp port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+*/
+
+	if err != nil {
+	  panic("failed to connect database")
+	}
+
+	defer db.Close()
+
+	db.AutoMigrate(&Parent{})
+	db.AutoMigrate(&Child{})
+
+	for index := range parents {
+		db.Create(&parents[index])
+	}
+
+	for index := range childs {
+		db.Create(&childs[index])
+	}
+
+	router.HandleFunc("/childs", GetChilds).Methods("GET")
+	router.HandleFunc("/childs/{id}", GetChild).Methods("GET")
+	router.HandleFunc("/parents/{id}", GetParent).Methods("GET")
+	router.HandleFunc("/childs/{id}", DeleteChild).Methods("DELETE")
+
+	handler := cors.Default().Handler(router)
+	
+	log.Fatal(http.ListenAndServe(":8080", handler))
+}
+
+func GetChilds(w http.ResponseWriter, r *http.Request) {	
+	var childs []Child
+	db.Find(&childs)
+	json.NewEncoder(w).Encode(&childs)
+}
+
+func GetChild(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var child Child
+	db.First(&child, params["id"])
+	json.NewEncoder(w).Encode(&child)
+}
+
+func GetParent(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var parent	Parent
+	var childs	[]Child
+	db.First(&parent, params["id"])
+	db.Model(&parent).Related(&childs)
+	parent.Childs = childs
+	json.NewEncoder(w).Encode(&parent)
+}
+
+func DeleteChild(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var child Child
+	db.First(&child, params["id"])
+	db.Delete(&child)
+
+	var childs []Child
+	db.Find(&childs)
+	json.NewEncoder(w).Encode(&childs)
+}
