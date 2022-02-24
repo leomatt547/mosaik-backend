3d62c95bb65422d72465ff9b3f17ffec1a0f1704
leo.matt.547 <leo.matt.547@gmail.com>
2022-02-24 12:51:36 +0700

    fixing index.html

--- src/api/controllers/home_controller.go
+++ src/api/controllers/home_controller.go
@@ -1,12 +1,15 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
-
-	"gitlab.informatika.org/if3250_2022_37_mosaik/mosaik-backend/src/api/responses"
 )
 
 func (server *Server) Home(w http.ResponseWriter, r *http.Request) {
-	//cors.EnableCors(&w)
-	responses.JSON(w, http.StatusOK, "Welcome To Mosaik API")
+	fmt.Println(r.URL.Path)
+	p := "." + r.URL.Path
+	if p == "./" {
+		p = "./index.html"
+	}
+	http.ServeFile(w, r, p)
 }
