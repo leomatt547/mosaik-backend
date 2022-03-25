415b2a85bd9b0fda906310fcd28ec989ea6f0020
leo.matt.547 <leo.matt.547@gmail.com>
2022-03-25 14:12:13 +0700

    push function nsfw

--- src/api/controllers/nsfw_urls_controller.go
+++ src/api/controllers/nsfw_urls_controller.go
@@ -15,6 +15,16 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type Result struct {
+	URL       string
+	isBlocked bool
+}
+
+type AIResult []struct {
+	ClassName   string  `json:"className"`
+	Probability float64 `json:"probability"`
+}
+
 func (server *Server) CreateNSFWUrl(w http.ResponseWriter, r *http.Request) {
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -69,29 +79,18 @@ func (server *Server) GetNSFWUrl(w http.ResponseWriter, r *http.Request) {
 
 func (server *Server) SavedSearchChecker(w http.ResponseWriter, r *http.Request) {
 	//cors.EnableCors(&w)
-	// vars := mux.Vars(r)
-	// uid, err := strconv.ParseUint(vars["id"], 10, 32)
-	// if err != nil {
-	// 	responses.ERROR(w, http.StatusBadRequest, err)
-	// 	return
-	// }
-	var kalimat []string
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		responses.ERROR(w, http.StatusUnprocessableEntity, err)
 		return
 	}
 
-	// parent := models.Parent{}
 	nsfw := models.NSFWUrl{}
 	err = json.Unmarshal(body, &nsfw)
 	if err != nil {
 		responses.ERROR(w, http.StatusUnprocessableEntity, err)
 		return
 	}
-
-	kalimat = append(kalimat, "Parsing : "+nsfw.Url)
-	// log.Println("Parsing : ", nsfw.Url)
 
 	// Request the HTML page.
 	resp, err := http.Get(nsfw.Url)
@@ -113,10 +112,46 @@ func (server *Server) SavedSearchChecker(w http.ResponseWriter, r *http.Request)
 
 	imageRegExp := regexp.MustCompile(`<img[^>]+\bsrc=["']([^"']+)["']`)
 
+	hasil_final := Result{}
+	hasil_final.URL = nsfw.Url
 	subMatchSlice := imageRegExp.FindAllStringSubmatch(string(htmlData), -1)
 	for _, item := range subMatchSlice {
-		kalimat = append(kalimat, "Image found : "+item[1])
+		//kalimat = append(kalimat, "Image found : "+item[1])
+		url := "https://mosaik-ai.herokuapp.com/api/image/classify?url=" + string(item[1])
+		method := "GET"
+
+		client := &http.Client{}
+		req, err := http.NewRequest(method, url, nil)
+
+		if err != nil {
+			responses.ERROR(w, http.StatusUnprocessableEntity, err)
+			return
+		}
+		res, err := client.Do(req)
+		if err != nil {
+			responses.ERROR(w, http.StatusUnprocessableEntity, err)
+			return
+		}
+		defer res.Body.Close()
+
+		res_ai := AIResult{}
+		err = json.NewDecoder(res.Body).Decode(&res_ai)
+		if err != nil {
+			responses.ERROR(w, http.StatusUnprocessableEntity, err)
+			return
+		}
+		// fmt.Printf("%s", j)
+		for _, hasil := range res_ai {
+			if hasil.ClassName == "Porn" || hasil.ClassName == "Sexy" || hasil.ClassName == "Hentai" {
+				if hasil.Probability >= 30 {
+					hasil_final.isBlocked = true
+					responses.JSON(w, http.StatusOK, hasil_final)
+					return
+				}
+			}
+		}
 		// log.Println("Image found : ", item[1])
 	}
-	responses.JSON(w, http.StatusOK, kalimat)
+	hasil_final.isBlocked = false
+	responses.JSON(w, http.StatusOK, hasil_final)
 }
