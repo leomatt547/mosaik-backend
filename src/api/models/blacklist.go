a148ac6e965e3d246edb24143e827ef138e415c3
Hughie Alghaniyyu Emiliano <68457533+HughieAlghani@users.noreply.github.com>
2022-04-06 13:03:47 +0700

    add blacklist and whitelist

--- src/api/models/blacklist.go
+++ src/api/models/blacklist.go
@@ -3,21 +3,22 @@ package models
 import (
 	"errors"
 	"time"
+
+	"github.com/jinzhu/gorm"
 )
 
 type Blacklist struct {
 	ID        uint64    `gorm:"primary_key;auto_increment" json:"id"`
-	UrlID     uint64    `gorm:"size:255;not null;" json:"url_id"`
-	Url       Url       `json:"Url"`
-	ParentID  uint32    `gorm:"not null" json:"parent_id"`
+	Url       string    `gorm:"uniqueIndex:blacklists_url_parent_id;type:text;not null;" json:"url"`
+	ParentID  uint32    `gorm:"uniqueIndex:blacklists_url_parent_id;not null" json:"parent_id"`
 	Parent    Parent    `json:"Parent"`
 	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
 	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
 }
 
 func (bl *Blacklist) Validate() error {
-	if bl.UrlID == 0 {
-		return errors.New("butuh url_id")
+	if bl.Url == "" {
+		return errors.New("butuh url")
 	}
 	if bl.ParentID == 0 {
 		return errors.New("butuh parent_id")
@@ -25,4 +26,88 @@ func (bl *Blacklist) Validate() error {
 	return nil
 }
 
-// func (bl *Blacklist)
+func (bl *Blacklist) SaveBlacklist(db *gorm.DB) (*Blacklist, error) {
+	err := db.Debug().Create(&bl).Error
+	if err != nil {
+		return &Blacklist{}, err
+	}
+	if bl.ID != 0 {
+		err = db.Debug().Model(&Parent{}).Where("id = ?", &bl.ParentID).Take(&bl.Parent).Error
+		if err != nil {
+			return &Blacklist{}, err
+		}
+	}
+	return bl, err
+}
+
+func (bl *Blacklist) FindAllBlacklist(db *gorm.DB) (*[]Blacklist, error) {
+	list := []Blacklist{}
+	err := db.Debug().Model(&Blacklist{}).Limit(100).Find(&bl).Error
+	if err != nil {
+		return &[]Blacklist{}, err
+	}
+	if len(list) > 0 {
+		for i := range list {
+			err := db.Debug().Model(&Parent{}).Where("id = ?", list[i].ParentID).Take(&list[i].Parent).Error
+			if err != nil {
+				return &[]Blacklist{}, err
+			}
+		}
+	}
+	return &list, err
+}
+
+func (bl *Blacklist) FindBlacklistByID(db *gorm.DB, uid uint64) (*Blacklist, error) {
+	err := db.Debug().Model(Blacklist{}).Where("id = ?", uid).Take(&bl).Error
+	if err != nil {
+		return &Blacklist{}, err
+	}
+
+	if gorm.IsRecordNotFoundError(err) {
+		return &Blacklist{}, errors.New("Blacklist Not Found")
+	}
+	if bl.ID != 0 {
+		err = db.Debug().Model(&Parent{}).Where("id = ?", &bl.ParentID).Take(&bl.Parent).Error
+		if err != nil {
+			return &Blacklist{}, err
+		}
+	}
+	return bl, err
+}
+
+func (bl *Blacklist) FindBlacklistByParentID(db *gorm.DB, pid uint32) (*[]Blacklist, error) {
+	list := []Blacklist{}
+	err := db.Debug().Model(&Blacklist{}).Limit(100).Where("parent_id = ?", pid).Find(&list).Error
+	if err != nil {
+		return &[]Blacklist{}, err
+	}
+	if len(list) > 0 {
+		for i := range list {
+			err := db.Debug().Model(&Parent{}).Where("id = ?", list[i].ParentID).Take(&list[i].Parent).Error
+			if err != nil {
+				return &[]Blacklist{}, err
+			}
+		}
+	}
+	return &list, nil
+}
+
+func (bl *Blacklist) FindRecordByUrl(db *gorm.DB, link string) (*[]Blacklist, error) {
+	list := []Blacklist{}
+	err := db.Debug().Model(&Blacklist{}).Where("url = ?", link).Find(&list).Error
+	if err != nil {
+		return &[]Blacklist{}, err
+	}
+	if gorm.IsRecordNotFoundError(err) {
+		return &[]Blacklist{}, errors.New("Blacklist Not Found")
+	}
+	if len(list) > 0 {
+		for i := range list {
+			err := db.Debug().Model(&Parent{}).Where("id = ?", list[i].ParentID).Take(&list[i].Parent).Error
+			if err != nil {
+				return &[]Blacklist{}, err
+			}
+		}
+	}
+	return &list, err
+}
---
+++ src/api/models/whitelist.go
@@ -0,0 +1,113 @@
+package models
+
+import (
+	"errors"
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
+
+type Whitelist struct {
+	ID        uint64    `gorm:"primary_key;auto_increment" json:"id"`
+	Url       string    `gorm:"uniqueIndex:whitelists_url_parent_id;type:text;not null;" json:"url"`
+	ParentID  uint32    `gorm:"uniqueIndex:whitelists_url_parent_id;not null" json:"parent_id"`
+	Parent    Parent    `json:"Parent"`
+	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+}
+
+func (wl *Whitelist) Validate() error {
+	if wl.Url == "" {
+		return errors.New("butuh url")
+	}
+	if wl.ParentID == 0 {
+		return errors.New("butuh parent_id")
+	}
+	return nil
+}
+
+func (wl *Whitelist) SaveWhitelist(db *gorm.DB) (*Whitelist, error) {
+	err := db.Debug().Create(&wl).Error
+	if err != nil {
+		return &Whitelist{}, err
+	}
+	if wl.ID != 0 {
+		err = db.Debug().Model(&Parent{}).Where("id = ?", &wl.ParentID).Take(&wl.Parent).Error
+		if err != nil {
+			return &Whitelist{}, err
+		}
+	}
+	return wl, err
+}
+
+func (wl *Whitelist) FindAllWhitelist(db *gorm.DB) (*[]Whitelist, error) {
+	list := []Whitelist{}
+	err := db.Debug().Model(&Whitelist{}).Limit(100).Find(&wl).Error
+	if err != nil {
+		return &[]Whitelist{}, err
+	}
+	if len(list) > 0 {
+		for i := range list {
+			err := db.Debug().Model(&Parent{}).Where("id = ?", list[i].ParentID).Take(&list[i].Parent).Error
+			if err != nil {
+				return &[]Whitelist{}, err
+			}
+		}
+	}
+	return &list, err
+}
+
+func (wl *Whitelist) FindWhitelistByID(db *gorm.DB, uid uint64) (*Whitelist, error) {
+	err := db.Debug().Model(Whitelist{}).Where("id = ?", uid).Take(&wl).Error
+	if err != nil {
+		return &Whitelist{}, err
+	}
+
+	if gorm.IsRecordNotFoundError(err) {
+		return &Whitelist{}, errors.New("Whitelist Not Found")
+	}
+	if wl.ID != 0 {
+		err = db.Debug().Model(&Parent{}).Where("id = ?", &wl.ParentID).Take(&wl.Parent).Error
+		if err != nil {
+			return &Whitelist{}, err
+		}
+	}
+	return wl, err
+}
+
+func (wl *Whitelist) FindWhitelistByParentID(db *gorm.DB, pid uint32) (*[]Whitelist, error) {
+	list := []Whitelist{}
+	err := db.Debug().Model(&Whitelist{}).Limit(100).Where("parent_id = ?", pid).Find(&list).Error
+	if err != nil {
+		return &[]Whitelist{}, err
+	}
+	if len(list) > 0 {
+		for i := range list {
+			err := db.Debug().Model(&Parent{}).Where("id = ?", list[i].ParentID).Take(&list[i].Parent).Error
+			if err != nil {
+				return &[]Whitelist{}, err
+			}
+		}
+	}
+	return &list, nil
+}
+
+func (wl *Whitelist) FindRecordByUrl(db *gorm.DB, link string) (*[]Whitelist, error) {
+	list := []Whitelist{}
+	err := db.Debug().Model(&Whitelist{}).Where("url = ?", link).Find(&list).Error
+	if err != nil {
+		return &[]Whitelist{}, err
+	}
+	if gorm.IsRecordNotFoundError(err) {
+		return &[]Whitelist{}, errors.New("Whitelist Not Found")
+	}
+	if len(list) > 0 {
+		for i := range list {
+			err := db.Debug().Model(&Parent{}).Where("id = ?", list[i].ParentID).Take(&list[i].Parent).Error
+			if err != nil {
+				return &[]Whitelist{}, err
+			}
+		}
+	}
+	return &list, err
+}
